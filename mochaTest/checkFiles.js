d0b4661f2393974d7f60fce85d81aab9983ad1cd
Chad-h4kNtosh <chadkoslovsky@gmail.com>
2018-08-29 13:22:48 -0600

    Added async mocha test.

--- mochaTest/checkFiles.js
+++ mochaTest/checkFiles.js
@@ -4,7 +4,9 @@ const fs = require('fs'),
 
 var assert = require('assert');
 describe('Check Files', function () {
+
   describe('Check custom.js file', function () {
+
     describe('Check if custom.js is within template folder', function () {
       it('Should return true', function (done) {
 
@@ -13,6 +15,7 @@ describe('Check Files', function () {
 
       });
     });
+
     describe('Check if custom.js moved to public js file.', function () {
       it('Should return true', function (done) {
 
@@ -21,5 +24,45 @@ describe('Check Files', function () {
 
       });
     });
+
+    describe('Check if custom.js varibles were changed', function () {
+      it('Should not return error', function (done) {
+
+        var readFile = () => {
+
+          try {
+
+          } catch(e) {
+            
+          }
+
+        fs.readFile('./public/js/custom.js', 'utf8', function (err, data) {
+
+          if (err) {
+            throw err;
+          } 
+          var regex = / = '{{(\w+}}';)/g;
+          if(data.match(regex)) {
+            throw new TypeError('Found varible within file custom.js');
+          } else {
+
+
+            done();
+          }
+//          throw new TypeError('Illegal salmon!');
+           
+          
+
+          
+        })
+        
+      }
+        chai.expect(readFile).to.not.throw();
+
+
+      });
+    });
   });
 });
+
+
