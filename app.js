1e732f6c3fa3bc87585794dffb2885034f97a42c
Chad-h4kNtosh <chadkoslovsky@gmail.com>
2018-08-30 02:32:37 -0600

    Added https on node level. Added checks for secure routes.

--- app.js
+++ app.js
@@ -5,17 +5,29 @@ const express = require('express'),
   fs = require('fs'),
   path = require('path'),
   URL = require('url-parse'),
+
+
+
+
+  
   http = require('http'),
   https = require('https'),
   server = http.createServer(app),
-  serverSecure = https.createServer(app),
   io = require('socket.io')(server),
   argv = require('yargs').argv,
-  port = 2022,
   cookieSession = require('cookie-session'),
   ip = require("ip"),
-  templateLoop = require("./controllers/templateLoop.js"),
   config = require('./config/config');
+
+  require("./controllers/templateLoop.js");
+
+
+  let httpsServerOptions = {
+    'key': fs.readFileSync('./webServer/https/key.pem'),
+    'cert': fs.readFileSync('./webServer/https/cert.pem')
+  }
+  let serverSecure = https.createServer(httpsServerOptions,app);
+
 
 
 let socketClients = new Object(),
@@ -36,36 +48,43 @@ let socketClients = new Object(),
       //start express server and then do callback after started (Mocha testing if test argument was provided)
       webServer(config.httpPort, version, server, app, function () {
 
-        if (argv.mochaTest || argv.mocha || argv.test || argv.mochatest) {
-          var Mocha = require('mocha'),
-            fs = require('fs'),
-            path = require('path');
+        webServer(config.httpsPort, version, serverSecure, app, function () {
 
-          // Instantiate a Mocha instance.
-          var mocha = new Mocha();
+          if (argv.mochaTest || argv.mocha || argv.test || argv.mochatest) {
+            var Mocha = require('mocha'),
+              fs = require('fs'),
+              path = require('path');
+  
+            // Instantiate a Mocha instance.
+            var mocha = new Mocha();
+  
+            var testDir = 'mochaTest'
+  
+            // Add each .js file to the mocha instance
+            fs.readdirSync(testDir).filter(function (file) {
+              // Only keep the .js files
+              return file.substr(-3) === '.js';
+  
+            }).forEach(function (file) {
+              mocha.addFile(
+                path.join(testDir, file)
+              );
+            });
+  
+            // Run the tests.
+            mocha.run(function (failures) {
+              process.exitCode = failures ? -1 : 0;  // exit with non-zero status if there were failures
+              //exit node ( Our script stays open because it has a web server. Need to exit so mocha test is finished...);
+              process.exit()
+            });
+            
+          };
+        });
 
-          var testDir = 'mochaTest'
 
-          // Add each .js file to the mocha instance
-          fs.readdirSync(testDir).filter(function (file) {
-            // Only keep the .js files
-            return file.substr(-3) === '.js';
-
-          }).forEach(function (file) {
-            mocha.addFile(
-              path.join(testDir, file)
-            );
-          });
-
-          // Run the tests.
-          mocha.run(function (failures) {
-            process.exitCode = failures ? -1 : 0;  // exit with non-zero status if there were failures
-            //exit node ( Our script stays open because it has a web server. Need to exit so mocha test is finished...);
-            process.exit()
-          });
-          
-        };
       });
+
+
     });
 
     require('./webServer/socket.io').socket(io);
--- mochaTest/checkRoutes.js
+++ mochaTest/checkRoutes.js
@@ -2,16 +2,20 @@ const isReachable = require('is-reachable'),
       fs = require('fs')
       ip = require('ip'),
       http = require('http'),
+      https = require('https'),
       config = require('./../config/config.js'),
       chai = require('chai');
 
-   
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 describe('Check All Server Routes', function () {
 
   describe('Check Node Route and Ports', function () {
 
+    describe('Check Unsecure Routes', function () {
+
     describe(`Check http ${ip.address()} with port ${config.httpPort}`, function () {
-      console.log(`${ip.address()}:${config.httpPort}`)
+
       it('Should return true', function () {
 
         return isReachable(`${ip.address()}:${config.httpPort}`).then(reachable => {
@@ -25,7 +29,8 @@ describe('Check All Server Routes', function () {
 
     });
 
-    describe(`Check http//${ip.address()}:${config.httpPort}/page/not/found`, function () {
+
+    describe(`Check http//${ip.address()}:${config.httpPort}/page/not/found. Catch all`, function () {
 
       it('Should return 404', function (done) {
 
@@ -41,6 +46,84 @@ describe('Check All Server Routes', function () {
 
 
     });
+
+    describe(`Check http//${ip.address()}:${config.httpPort}/`, function () {
+
+      it('Should return 404', function (done) {
+
+        var options = {method: 'GET', host: `${ip.address()}`, port: config.httpPort, path: '/'};
+        var req = http.request(options, function(res) {
+          chai.expect(res.statusCode).to.equal(200);
+
+            done();
+          });
+       req.end();
+    
+      });
+
+
+    });
+
+  });
+
+  describe('Check Secure Routes', function () {
+
+    describe(`Check https secure ${ip.address()} with port ${config.httpsPort}`, function () {
+
+      it('Should return true', function () {
+
+        return isReachable(`https://${ip.address()}:${config.httpsPort}`).then(reachable => {
+
+          chai.expect(reachable).to.be.true
+           
+        })
+    
+      });
+
+
+    });
+
+    describe(`Check https//${ip.address()}:${config.httpsPort}/page/not/found Catch all`, function () {
+
+      it('Should return 404', function (done) {
+        
+        var options = {method: 'GET', host: `${ip.address()}`, port: config.httpsPort, path: '/page/not/found'};
+        var req = https.request(options, function(res) {
+ 
+            chai.expect(res.statusCode).to.equal(404);
+          
+
+
+            done();
+          });
+       req.end();
+    
+      });
+
+
+    });
+
+    describe(`Check https//${ip.address()}:${config.httpsPort}/`, function () {
+
+      it('Should return 200', function (done) {
+        
+        var options = {method: 'GET', host: `${ip.address()}`, port: config.httpsPort, path: '/'};
+        var req = https.request(options, function(res) {
+ 
+            chai.expect(res.statusCode).to.equal(200);
+          
+
+
+            done();
+          });
+       req.end();
+    
+      });
+
+
+    });
+
+  });
 
   });
 
