b48b63cba03da88d171da2d22d570ff0f8b29017
Chad-h4kNtosh <chadkoslovsky@gmail.com>
2018-09-17 01:14:02 -0600

    Added mocha user lookup schema

--- mochaTest/checkMongo.js
+++ mochaTest/checkMongo.js
@@ -2,8 +2,11 @@ const fs = require('fs'),
       config = require('./../config/config.js'),
       mongoose = require('mongoose'),
       User = require('../webServer/models/User'),
-      DB = require('../webServer/mongoose');
+      DB = require('../webServer/mongoose'),
+
       chai = require('chai');
+
+      let createdUserId;
 
       mongoose.connection.dropDatabase(config.envName + '-' + config.databaseName);
 
@@ -24,7 +27,7 @@ describe('Mocha test for Mongo database checks', () =>  {
   });
 
   describe('Check to see if you can create user. With mongoose Schema.', () =>  {
-    it('Should return 1, 0 means closed.', (done) =>  {
+    it('Should return mongo user schema/created user.', (done) =>  {
 
       let createUser = new DB.User({
         _id: new DB.mongoose.Types.ObjectId(),
@@ -42,9 +45,27 @@ describe('Mocha test for Mongo database checks', () =>  {
           chai.expect(err).to.contain.keys('name', 'biography', 'passwordHash', 'created', 'updated', '__v', '_id');
           done();
         }
+        createdUserId = user._id;
         chai.expect(user._doc).to.contain.keys('name', 'biography', 'passwordHash', 'created', 'updated', '__v', '_id');
         done();
       })
+
+    });
+
+  });
+
+  describe('look up newly created user', () =>  {
+    it('Should return mongo user schema/look up of user', (done) =>  {
+
+      User.findOne({'_id': createdUserId}, function (err, user){
+        if(err) {
+          chai.expect('Could not find user: ' + createdUserId).to.be.false;
+          done();
+        }
+
+        chai.expect(user._doc).to.contain.keys('name', 'biography', 'passwordHash', 'created', 'updated', '__v', '_id');
+        done();
+      });
 
     });
 
