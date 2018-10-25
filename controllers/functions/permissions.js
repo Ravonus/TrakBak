3d2967e2cb900c5f01e2a2527579892f11131f89
Chad-h4kNtosh <chadkoslovsky@gmail.com>
2018-10-24 23:46:07 -0600

    Added match check for permission and groups for sockets/api function.

--- controllers/functions/permissions.js
+++ controllers/functions/permissions.js
@@ -4,7 +4,7 @@ module.exports = (num) => {
 
     promise: (userObj, policies, type) => {
 
-      if(!type) {
+      if (!type) {
         type = 'api';
       }
 
@@ -12,17 +12,17 @@ module.exports = (num) => {
       return new Promise((response, reject) => {
 
         if (policies && policies[type] && policies.permissions && policies.permissions > 0 || policies && policies[Object.keys(policies)].permissions && policies[Object.keys(policies)].permissions > 0 && userObj && userObj.permissions && policies[Object.keys(policies)][type]) {
-        
+
           var weight = 0;
           var permission;
-     
+
           if (!policies[type] && typeof policies[Object.keys(policies)].permissions === 'object') {
             weight = policies[Object.keys(policies)].permissions.weight;
             permission = policies[Object.keys(policies)].permissions.value;
           } else {
-           
-            if(policies[type] !== undefined){
-           
+
+            if (policies[type] !== undefined) {
+
               permission = policies.permissions;
             } else {
               permission = policies[Object.keys(policies)].permissions;
@@ -43,65 +43,109 @@ module.exports = (num) => {
                 var number = '1'.padEnd(i, '0');
 
                 binaryArray[parseInt(number, 2)] = true;
+
               }
+
               if (i === binaries.length) {
                 //    console.log(binaryArray);
                 //   response(binaryArray);
+
                 return binaryArray;
 
               }
             }
           }
           var userPerms = getBinary(num);
+
           var policyPerms = getBinary(permission);
 
-
-
-
           var promises = [];
+          var foundGroup = false;
+          var foundPerm = false;
           Object.keys(policyPerms).forEach((key, index) => {
 
-           
             if (policies[type] === undefined && policies[Object.keys(policies)].match && policies[Object.keys(policies)].match.length > 0) {
- 
 
-              var compare = policies[Object.keys(policies)].match;
+              if (policies[Object.keys(policies)].match) {
+                var compare = policies[Object.keys(policies)].match;
+              } else {
+                var compare = policies[Object.keys(policies)].groups
+                compare.push(policies[Object.keys(policies)].permissions)
+              }
+
               var userGroups = userObj.groups;
               var i;
+              var arr = [userPerms];
+              var addArr = [...userObj.groups, ...[userObj.permissions]];
+              for (i = 0; i < addArr.length; i++) {
 
-              for (i = 0; i < userGroups.length; i++) {
-                console.log('compare', compare[i])
-                if (typeof compare[i] === 'string' && compare.includes(userGroups[i].name)) {
 
+
+                if (typeof addArr[i] === 'object' && addArr[i].name && compare.includes(addArr[i].name)) {
+
+                  foundGroup = true;
                   promisePush(userPerms[key]);
-                 i = userObj.groups.length + 1;
-                } else if(typeof compare[i] === 'number') {
-                  console.log('mah group W00t compare numbers?', userGroups[i].name, ' real group ', groups)
+                  i = addArr[i].length + 1;
+
+                } else if (typeof addArr[i] !== 'object' && typeof addArr[i] === 'number') {
+
+                  if (compare.length > 1) {
+                    var str = `${compare.toString()},`;
+
+                    var match = str.match(/,\d+,/g);
+
+                  }
+                  match.forEach((num) => {
+                    num = Number(num.replace(/,/g, ''));
+                    var binary = getBinary(num);
+
+                    Object.keys(binary).forEach((check, index) => {
+                      if (userPerms[check]) {
+                        foundPerm = true;
+                      }
+
+                      if (index === Object.keys(binary).length - 1) {
+
+                        if (foundPerm) {
+                          i = addArr[i].length + 1;
+                          response('match perms')
+                        } else {
+                          reject('failed Matchzz')
+                        }
+                      }
+
+                    })
+                  })
+
                 } else {
-                  console.log('fuccc me', typeof compare[i])
-                  promises.push(new Promise((response, reject) => { 
-                    reject('failed Match')
-                  }))
+                  console.log('i cry', addArr[i])
+
+                  if (i === addArr.length - 1) {
+                    console.log('i', i, "addArr", addArr.length)
+                    if (foundPerm || foundGroup) {
+                      response('Found Match')
+                    } else {
+                      reject('failed Matchz')
+                    }
+
+                  }
+
                 }
               }
             } else {
-              console.log('USERPERMS', userPerms, policies[type], 'KEY', key)
+
               promisePush(userPerms[key])
             }
 
             function promisePush(perms) {
 
-              console.log('mah perms', perms)
-
               promises.push(new Promise((response, reject) => {
 
                 if (perms) {
-         
 
                   response('done')
                 } else {
 
-               
                   reject('fucc');
                 }
 
@@ -110,7 +154,6 @@ module.exports = (num) => {
 
             if (index >= Object.keys(policyPerms).length - 1) {
 
-              console.log('be promsies', promises)
               promise(promises);
             }
           })
@@ -121,7 +164,7 @@ module.exports = (num) => {
               .then(data => {
                 response('true')
               }).catch(err => {
-                console.log('wtd perms')
+
                 reject('fucc')
               })
           }
@@ -131,18 +174,15 @@ module.exports = (num) => {
         }
       })
 
-    
-
     },
 
     pf: (done) => {
       if (promise.promise()) {
 
-        console.log(promise.promise())
         return promise.promise().then((data) => {
           return done(null, data)
         }).catch(err => {
-          console.log('diz err', err)
+
           done(err);
         })
       }
